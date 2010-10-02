16afe7ace697a0b4ef341133a364b2cbbf9007b3
Kousuke Ebihara <ebihara@tejimaya.com>
2010-10-02 11:55:34 +0900

    added ability to listen request and to write response by porting from my dusty old code

--- Command/StartCommand.php
+++ Command/StartCommand.php
@@ -14,6 +14,10 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StartCommand extends Command
 {
+  protected
+    $socket,
+    $connections;
+
   protected function configure()
   {
     $this
@@ -24,6 +28,73 @@ class StartCommand extends Command
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $output->writeln('*WIP*'.PHP_EOL);
+    $this->socket = socket_create_listen(8080);
+    if (!$this->socket)
+    {
+      throw new \RuntimeException(sprintf('Failed to start server: %s', socket_last_error($this->socket)));
+    }
+
+    $this->listen();
+  }
+
+  protected function listen()
+  {
+    while (true)
+    {
+      $client = $this->getConnection();
+      if ($client)
+      {
+        $pid = pcntl_fork();
+
+        if ($pid)
+        {
+          $this->connections[$pid] = $client;
+        }
+        else
+        {
+          $content = 'Hello';
+
+          $response = "HTTP/1.0 200 OK\n"
+                    . "Content-Length: ".strlen($content)."\n"
+                    . "Content-Type: text/html\n"
+                    . "\n"
+                    . $content;
+
+          socket_write($client, $response, strlen($response));
+
+          exit;
+        }
+      }
+
+      $this->cleanConnections();
+    }
+  }
+
+  protected function getConnection()
+  {
+    $sockets = array($this->socket);
+    $client = null;
+    $w = $e = null;
+
+    if (socket_select($sockets, $w, $e, 0))
+    {
+      $client = socket_accept($this->socket);
+    }
+    else
+    {
+      usleep(100);
+    }
+
+    return $client;
+  }
+
+  protected function cleanConnections()
+  {
+    while (($child = pcntl_wait($status, WNOHANG)) > 0)
+    {
+      socket_shutdown($this->connections[$child]);
+      socket_close($this->connections[$child]);
+      unset($this->connections[$child]);
+    }
   }
 }
