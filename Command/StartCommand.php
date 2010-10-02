c0bcb372a80b2ecb6c788e09244c809f426fa222
Kousuke Ebihara <ebihara@tejimaya.com>
2010-10-03 00:23:37 +0900

    added ability to parse HTTP header and messages (partly, and I don't care of GET and POST parmeter)

--- Command/StartCommand.php
+++ Command/StartCommand.php
@@ -16,7 +16,9 @@ class StartCommand extends Command
 {
   protected
     $socket,
-    $connections;
+    $connections,
+
+    $request;
 
   protected function configure()
   {
@@ -26,7 +28,86 @@ class StartCommand extends Command
       ->setHelp('*WIP*');
   }
 
-  protected function processUserRequest()
+  protected function parseRequest($socket)
+  {
+    $raw = '';
+    $begin = time();
+
+    $headers = array();
+    $method = $uri = $version = $body = '';
+
+    $isTimedOut = true;
+
+    while (time() < $begin + 30)
+    {
+      $data = socket_read($socket, 4096, PHP_BINARY_READ);
+      $raw = $raw.$data;
+
+      if (empty($headers) && $this->isCompletedHeader($raw))
+      {
+        list($method, $uri, $version, $headers) = $this->splitHeaderAndBody($raw);;
+      }
+
+      if (false === $data)
+      {
+        return false;
+      }
+      elseif (!$raw)  // I don't care POST yet
+      {
+        $isTimedOut = false;
+        $body = $raw;
+
+        break;
+      }
+    }
+
+    if ($isTimedOut)
+    {
+      throw new RuntimeException('Timed out');
+    }
+
+    return array($method, $uri, $version, $headers, $body);
+  }
+
+  protected function isCompletedHeader($response)
+  {
+    return (bool)preg_match('/(\n\s+\n)/', $response);
+  }
+
+  protected function splitHeaderAndBody(&$raw)
+  {
+    $headers = array();
+    $method = $uri = $version = '';
+
+    $parts = preg_split('/(\n\s+\n)/', $raw, 2);
+
+    // body
+    $raw = $parts[1];
+
+    // parse headers
+    $rawHeaders = explode("\n", $parts[0]);
+    foreach ($rawHeaders as $i => $v)
+    {
+      if (0 == $i)
+      {
+        list($method, $uri, $version) = explode(' ', $v);
+
+
+        continue;
+      }
+
+      $header = explode(":", $v, 2);
+
+      if (count($header) == 2)
+      {
+        $headers[trim($header[0])] = trim($header[1]);
+      }
+    }
+
+    return array($method, $uri, $version, $headers);
+  }
+
+  protected function processUserHttpRequest($method, $uri, $version, $headers, $body)
   {
     restore_error_handler();
 
@@ -37,7 +118,7 @@ class StartCommand extends Command
     require_once $r->getFileName();
 
     $requestClass = $originalKernel->getContainer()->getRequestService();
-    $request = $requestClass::create('/statuses');
+    $request = $requestClass::create($uri, strtolower($method));
     $request->setRequestFormat('html');
 
     $newKernel = new $kernelClass('prod', true);
@@ -74,7 +155,9 @@ class StartCommand extends Command
         }
         else
         {
-          $response = $this->processUserRequest();
+          list($method, $uri, $version, $headers, $body) = $this->parseRequest($client);
+
+          $response = $this->processUserHttpRequest($method, $uri, $version, $headers, $body);
 
           socket_write($client, $response, strlen($response));
 
